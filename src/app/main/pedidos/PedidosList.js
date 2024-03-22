6864684917dd2ffb21f3344a6fd3c3de7bb978ad
Lucas Lacerda <lucas.lacerda@meuvista.com>
2024-03-22 02:07:40 -0300

    Update PedidosList.js

--- src/app/main/pedidos/PedidosList.js
+++ src/app/main/pedidos/PedidosList.js
@@ -442,7 +442,7 @@ export default function EnhancedTable() {
     console.log('apiURL', apiURL);
 
     function handleClickRow(rowId) {
-        navigate(`/pedidos/${rowId}`);
+        navigate(`/pedido/${rowId}`);
     }
     return (
         <Box sx={{ width: '100%', padding: '2rem' }}>
