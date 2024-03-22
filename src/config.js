1010128d2598e1f799f6af1c7a8b4fac03f68806
Lucas Lacerda <lucas.lacerda@meuvista.com>
2024-03-22 00:32:44 -0300

    .

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@
 export default function getConfigAPI() {
     const env = process.env.NODE_ENV || 'development';
 
-    return { API_URL: `${process.env.REACT_APP_API_ORIGIN || 'http://localhost:4000'}/api`, APP_URL: `${process.env.REACT_APP_ORIGIN || 'https://app.hallonlab.com.br'}` };
+    return { API_URL: `${process.env.REACT_APP_API_ORIGIN || 'http://localhost:4000'}/api`, APP_URL: `${process.env.REACT_APP_ORIGIN || 'http://localhost:8000'}` };
 }
